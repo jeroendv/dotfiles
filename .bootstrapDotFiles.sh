b191aa239b990e70e6534036ceed9b9ff260eb5b
jeroendv <fly.jdv@gmail.com>
2020-02-04 01:02:43 +0100

    bootstrapper script

--- .bootstrapDotFiles.sh
+++ .bootstrapDotFiles.sh
@@ -1,8 +1,18 @@
 #!/usr/bin/bash
 
-# source this scrip to bootstrap dotfile management
+# fetch script then source it to bootstrap dotfile management
 # 
+# 	$ wget https://raw.githubusercontent.com/jeroendv/dotfiles/master/.bootstrapDotFiles.sh 
 # 	$ . bootstrapDotFiles.sh
+
+# clone the bare dotfiles repo
+cd ~
+git clone --bare https://github.com/jeroendv/dotfiles.git .dotfiles
+
+
+# reset to master branch without changing the working copy
+git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME reset --mixed
+
 
 
 # make the g script executable
@@ -12,3 +22,6 @@ chmod u+x g
 
 # don't list untracked files in the status of my dotfiles repo
 ./g  config --local status.showUntrackedFiles no
+
+# list local differences with managed dotfiles.
+./g status
